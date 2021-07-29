d97c7b828a654b8f8b85380c00c2a1c50747d8d9
Vinicius Nunes Teixeira Da Silva <66691283+viniciusnunhez27@users.noreply.github.com>
2021-07-29 20:29:53 -0300

    Ajuster no alinhamento pagina

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
 const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
+const recipes = document.querySelectorAll('.recipe-content');
+const modal = document.querySelector('.modal');
 
-for (let card of cards) {
-  card.addEventListener("click", function () {
-    modalOverlay.classList.add('active');
-  });
+for (let recipe of recipes){
+    recipe.addEventListener("click", function(){
+        const imgId = recipe.getAttribute("id");
+        const tituloModal = recipe.querySelector("h3").textContent;
+        const autorModal = recipe.querySelector('p').textContent;
+        modalOverlay.classList.add('active');
+        modalOverlay.querySelector('img').src = `/Foodfy-Launchbase/assets/${imgId}`
+        modalOverlay.querySelector('h3').textContent = `${tituloModal}`
+        modalOverlay.querySelector('p').textContent = `${autorModal}`
+
+    })
 }
- 
-document.querySelector('.close-modal').addEventListener("click",function(){
-     modalOverlay.classList.remove('active');
+
+
+document.querySelector('.close-modal').addEventListener("click", function(){
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('img').src =""
 })
+
 
