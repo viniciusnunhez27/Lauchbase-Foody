2281cca22cb730b89fdb54133e2d43eba98debf9
Vinicius Nunes Teixeira Da Silva <66691283+viniciusnunhez27@users.noreply.github.com>
2021-07-31 19:34:44 -0300

    Modal Abre e fecha.

--- scripts.js
+++ scripts.js
@@ -1,24 +1,14 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const recipes = document.querySelectorAll('.recipe-content');
-const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card-content')
+const modal = document.querySelector('.modal')
 
-for (let recipe of recipes){
-    recipe.addEventListener("click", function(){
-        const imgId = recipe.getAttribute("id");
-        const tituloModal = recipe.querySelector("h3").textContent;
-        const autorModal = recipe.querySelector('p').textContent;
-        modalOverlay.classList.add('active');
-        modalOverlay.querySelector('img').src = `/Foodfy-Launchbase/assets/${imgId}`
-        modalOverlay.querySelector('h3').textContent = `${tituloModal}`
-        modalOverlay.querySelector('p').textContent = `${autorModal}`
-
-    })
+for (let card of cards){ 
+   card.addEventListener("click", function(){
+       modalOverlay.classList.add('active')
+   })
 }
-
-
-document.querySelector('.close-modal').addEventListener("click", function(){
-    modalOverlay.classList.remove('active')
-    modalOverlay.querySelector('img').src =""
+    
+document.querySelector(".close-modal").addEventListener("click",function(){
+      modalOverlay.classList.remove("active")
 })
-
 
